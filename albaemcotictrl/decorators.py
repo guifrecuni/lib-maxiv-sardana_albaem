24d89e9cbc53b968f15ed3bb16ea83a6ea1e6184
Guifre Cuni <guifre.cuni@cells.es>
2017-03-23 18:32:42 +0100

    changing formatting

--- albaemcotictrl/decorators.py
+++ albaemcotictrl/decorators.py
@@ -20,28 +20,28 @@ def alert_problems(meth):
             return meth(self, *args, **kwargs)
 
         except PyTango.DevFailed:
-            header = 'PyTango.DevFailed in: {}\n'.format(meth.__name__)
-            print '{:-^79}'.format(header)
+            header = 'PyTango.DevFailed in: {0}\n'.format(meth.__name__)
+            print '{0:-^79}'.format(header)
 
             exctype, value = sys.exc_info()[:2]
             for error in value:
                 print 'Reason:\t{0}\nDescription:\t{1}\nOrigin:\t{2}\n'.format(
                        error.reason, error.desc, error.origin)
-            print '{:-^79}'.format('')
+            print '{0:-^79}'.format('')
 
         except PyTango.ConnectionFailed as e:
             error_msg = "Could not connect with the electrometer."
-            exception_msg = "Exception: {}".format(e)
+            exception_msg = "Exception: {0}".format(e)
             msg = '__init__(): {2}\n{1}'.format(error_msg, exception_msg)
             # self._log.error(msg)
-            print '{:-^79}'.format('Connection Failed')
-            print '{}'.format(msg)
-            print '{:-^79}'.format('')
+            print '{0:-^79}'.format('Connection Failed')
+            print '{0}'.format(msg)
+            print '{0:-^79}'.format('')
 
         except Exception:
-            header = 'Unnexpected Error in: {}'.format(meth.__name__)
-            print '{:-^79}'.format(header)
+            header = 'Unnexpected Error in: {0}'.format(meth.__name__)
+            print '{0:-^79}'.format(header)
             traceback.print_exc()
-            print '{:-^79}'.format('')
+            print '{0:-^79}'.format('')
 
     return _alert_problems
